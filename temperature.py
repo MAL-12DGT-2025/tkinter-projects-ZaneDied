8ad914c39c7e54d1f27b0edc02f2b09039b8e34d
ZaneDied <zane.abad@marcellin.school.nz>
2025-03-05 12:47:24 +1300

    woopsies

--- temperature.py
+++ temperature.py
@@ -7,4 +7,10 @@ title = tk.Label(root,text = "Temperature\nconverter").grid(row = 0,column = 0)
 number = tk.Entry(root).grid(row = 1, column = 0 )
 
 
+ctofr = tk.Radiobutton(root, text = "Celsius to Fahrenheit").grid(row = 2, column = 0)
+ftocr = tk.Radiobutton(root, text = "Fahrenheit to Celsius ").grid(row = 2, column = 1)
+
+convb = tk.Button(root, text = "Convert").grid(row = 3, column = 0)
+resl = tk.Label(root, text = "")
+
 root.mainloop()
