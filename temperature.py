61ed8ebd33fe2b0535f0a04e09d87b9f8894a34e
ZaneDied <zane.abad@marcellin.school.nz>
2025-03-05 13:09:22 +1300

    fixings

--- temperature.py
+++ temperature.py
@@ -1,16 +1,17 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
+rclick = tk.IntVar()
+
+title = ttk.Label(root,text = "Temperature converter").grid(row = 0,column = 0, columnspan=2, pady = 10, padx = 10)
+number = tk.Entry(root).grid(row = 1, column = 0,columnspan=2)
 
 
-title = tk.Label(root,text = "Temperature\nconverter").grid(row = 0,column = 0)
-number = tk.Entry(root).grid(row = 1, column = 0 )
+ctofr = ttk.Radiobutton(root, text = "Celsius to Fahrenheit", variable = rclick, value = 1).grid(row = 2, column = 0)
+ftocr = ttk.Radiobutton(root, text = "Fahrenheit to Celsius ", variable = rclick, value = 2).grid(row = 2, column = 1)
 
-
-ctofr = tk.Radiobutton(root, text = "Celsius to Fahrenheit").grid(row = 2, column = 0)
-ftocr = tk.Radiobutton(root, text = "Fahrenheit to Celsius ").grid(row = 2, column = 1)
-
-convb = tk.Button(root, text = "Convert").grid(row = 3, column = 0)
-resl = tk.Label(root, text = "")
+convb = ttk.Button(root, text = "Convert").grid(row = 3, column = 0, columnspan=2)
+resl = ttk.Label(root, text = "",).grid(row = 4, column= 0, columnspan=2)
 
 root.mainloop()
