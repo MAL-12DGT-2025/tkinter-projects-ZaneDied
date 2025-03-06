9a78f71fa202623a2c1648f6f7faf1fb4e5d7ff3
ZaneDied <zane.abad@marcellin.school.nz>
2025-03-07 10:41:25 +1300

    i dont know some lambda stuff

--- calculator.py
+++ calculator.py
@@ -5,80 +5,86 @@ root = tk.Tk()
 root.title("Calculator")
 
 lineq = ""
-
+results = ""
 def press(val):
     global lineq
-    lineq = lineq + str(val)
-    lineq.set()
-
-def eqpress():
-
-    try:
-
-        global lineq
+    global results
+    if val == "=":
         total = str(eval(lineq))
         result.config(text = total)
-        lineq = ""
-    except:
-        result.config(text = "ERROR")
+        lineq = total
+        results = total
         print(lineq)
+    elif val == "c":
+        lineq = ""
+        results = ""
+        result.config(text = "0")
+    else:
+        lineq = lineq + str(val)
+        if val == "*":
+            results = f"{results} x"
+        else:
+            results = f"{results} {val}"
+            
+        result.config(text = results)
 
 
-result = ttk.Label(root, text = "Null")
+
+result = ttk.Label(root, text = "0")
 result.grid(row = 0, column = 0, columnspan = 5, padx = 10, pady = 10, )
 
 btn7 = ttk.Button(root, text = "7", command= lambda : press(7))
 btn7.grid(row = 1 , column = 1)
 
-btn8 = ttk.Button(root, text = "8")
-btn8.grid(row = 1 , column = 2)
+btn8 = ttk.Button(root, text = "8", command= lambda : press(8))
+btn8.grid(row = 1 , column = 2,)
 
-btn9 = ttk.Button(root, text = "9")
+btn9 = ttk.Button(root, text = "9", command= lambda : press(9))
 btn9.grid(row = 1, column = 3)
 
-btndev = ttk.Button(root, text = "/")
+btndev = ttk.Button(root, text = "/", command= lambda : press("/"))
 btndev.grid(row = 1, column = 4)
 
 
-btn4 = ttk.Button(root, text = "4")
+btn4 = ttk.Button(root, text = "4", command= lambda : press(4))
 btn4.grid(row = 2, column = 1)
 
-btn5 = ttk.Button(root, text = "5")
+btn5 = ttk.Button(root, text = "5", command= lambda : press(5))
 btn5.grid(row = 2, column = 2)
 
-btn6 = ttk.Button(root, text = "6")
+btn6 = ttk.Button(root, text = "6", command= lambda : press(6))
 btn6.grid(row = 2, column = 3)
 
-btnx = ttk.Button(root, text = "x")
+btnx = ttk.Button(root, text = "x", command= lambda : press("*"))
 btnx.grid(row = 2, column = 4)
 
 
-btn1 = ttk.Button(root, text = "1")
+btn1 = ttk.Button(root, text = "1", command= lambda : press(1))
 btn1.grid(row = 3, column = 1)
 
-btn2 = ttk.Button(root, text = "2")
+btn2 = ttk.Button(root, text = "2", command= lambda : press(2))
 btn2.grid(row = 3, column = 2)
 
-btn3 = ttk.Button(root, text = "3")
+btn3 = ttk.Button(root, text = "3", command= lambda : press(3))
 btn3.grid(row = 3, column = 3)
 
-btnmin = ttk.Button(root, text = "-")
+btnmin = ttk.Button(root, text = "-", command= lambda : press("-"))
 btnmin.grid(row = 3, column = 4)
 
 
-btnc = ttk.Button(root, text = "C")
+btnc = ttk.Button(root, text = "C", command = lambda : press("c"))
 btnc.grid(row = 4, column = 1)
 
-btn0 = ttk.Button(root, text = "0")
+btn0 = ttk.Button(root, text = "0", command= lambda : press(0))
 btn0.grid(row = 4, column = 2)
 
-btndot = ttk.Button(root, text = ".")
+btndot = ttk.Button(root, text = ".", command= lambda : press("."))
 btndot.grid(row = 4, column = 3)
 
-btnpls = ttk.Button(root, text = "+")
+btnpls = ttk.Button(root, text = "+", command= lambda : press("+"))
 btnpls.grid(row = 4, column = 4)
 
-btneq = ttk.Button(root, text = "=")
+btneq = ttk.Button(root, text = "=", command= lambda : press("="))
 btneq.grid(row = 5, column = 1, columnspan = 5, ipadx = 115)
 
 
