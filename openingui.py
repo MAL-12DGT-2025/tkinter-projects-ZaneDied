f088eca22abad2dee0ad1dd0a72e2842d3c05bc9
ZaneDied <zane.abad@marcellin.school.nz>
2025-03-12 13:19:22 +1300

    finished la

--- openingui.py
+++ openingui.py
@@ -4,38 +4,63 @@ import os
 
 root = tk.Tk()
 root.title("File reader")
-
-def fsearch():
+count = 0
+fileexplore = ""
+def fsearch(chc,op):
         files = os.listdir()
         listf = ""
         for file in files:
             if file.endswith(".txt"):
-                listf = f"{file}\n{listf}" 
-        listf = listf.rstrip(listf[-5:])
-        return listf
-
+                listf = f"{file.rsplit('.', maxsplit=1)[0]}\n{listf}" 
+                
+        
+        if listf == "":
+                listf = "No files found"
+        elif chc == "get":
+            print(listf)
+            return listf
+        elif chc == "check":
+            if op in listf:
+                return True
+            else:
+                 return False
 def bpress(ty):
+    global count
+    global fileexplore
     if ty == "search":
-        returnl.config(text = fsearch())
+        count = 0
+        returnl.config(text = fsearch("get",None))
     if ty == "read":
+        count = 0
         try:
             f = open(f"{direc.get()}.txt", "r")
             returnl.config(text = f.read())
         except Exception as e:
             returnl.config(text=f"{type(e).__name__}")
     if ty == "write":
-        files = os.listdir()
-        listf = ""
-        for file in files:
-            if file.endswith(".txt"):
-                listf = f"{file}\n{listf}" 
-        listf = listf.rstrip(listf[-5:])
-        returnl.config(text = f"Please select a file {fsearch()}")
-        direc.set("")
-        
-        try:
-            f = open(f"{direc.get}.txt", "w")
-            
+        if count == 0:
+              if fsearch("get",None) != "No files found":
+                direc.set("")
+                returnl.config(text = f"Chose a file to edit and then press\n write again to start writing. \n{fsearch("get",None)}")
+                count += 1
+              else:
+                   direc.set("No files found.")
+        elif count == 1:
+            fileexplore = direc.get()
+            if fsearch("check",fileexplore):
+                direc.set("")
+                returnl.config(text = "Start typing to append into file \n once finish press write agian.")
+                count += 1
+            else:
+                 returnl.config(text = "No file found.")
+        elif count == 2:
+            f = open(f"{fileexplore}.txt","w")
+            f.write(direc.get())
+            f.close()
+            count = 0
+            direc.set("")
+            returnl.config(text = "Finished")
+
     
 direc = tk.StringVar()
 entryn = ttk.Entry(root, textvariable = direc)
