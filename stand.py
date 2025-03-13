6bb0b198d94923a8658d65718190209551596c34
ZaneDied <zane.abad@marcellin.school.nz>
2025-03-14 10:04:32 +1300

    asdasdasdadasdasdasdasd

--- stand.py
+++ stand.py
@@ -2,30 +2,59 @@ import tkinter as tk
 from tkinter import ttk 
 
 
-window = tk.Tk() 
-window.geometry('350x400') 
-window.title('Lemonade stand!!') 
+root = tk.Tk() 
+root.geometry('400x400') 
+root.title('Lemonade stand!!') 
+
+cartv = ""
+carv = ""
+def press(ops):
+    global carv
+    global cartv
+    menop = menu.get()
+    if ops == "add":
+        cartv =  f"{menop} + {cartv} "
+        carv =  str(eval(cartv))
+        cart.config(text = f"{cartv} = {carv}")
 
 
+ttk.Label(root, text = "Lemonade Stand").grid(row = 0, column = 1) 
+ttk.Label(root, text = "Select an item :").grid(row = 1, column = 0,  padx = 10, pady = 25) 
+cart = ttk.Label(root, text = "Cart: ")
+cart.grid(row = 5, column = 1)
 
-ttk.Label(window, text = "Lemonade Stand", font = 15).grid(row = 0, column = 1) 
+menu = ttk.Combobox(root, width = 27)
+menu.grid(row = 1 , column = 1)
 
+extra = ttk.Combobox(root, width = 27)
+extra.grid_remove()
 
-ttk.Label(window, text = "Select an item :").grid(column = 0, row = 5, padx = 10, pady = 25) 
+extra['values'] = (
 
+    "Extra sugar +$20%",                         
+    "Extra lemon +$10%",
+    "Medium cup +$30%",
+    "Large cup +$60%",             
 
-op = tk.StringVar() 
-menu = ttk.Combobox(window, width = 27, textvariable = op) 
-
+)
 
 menu['values'] = (
     
     "Lemonade drink $3",
-    "Extra sugar 20% more",                         
-    "Extra lemonade 10% more"                      
+    "Sprite Lemonade $5",
+    "Ice tea $4"     
+
 ) 
 
 
-menu.grid(column = 1, row = 5) 
-menu.current(1) 
-window.mainloop() 
+addb = ttk.Button(root, text = "Add", command = lambda : press("add"))
+addb.grid(row = 2, column = 0)
+
+removeb = ttk.Button(root, text = "Remove", command = lambda : press("remove"))
+removeb.grid(row = 3 , column = 0 )
+
+checkoutb = ttk.Button(root, text =  "Checkout", command = lambda : press("checkout"))
+checkoutb.grid(row = 4 , column = 0)
+
+menu.current(0) 
+root.mainloop() 
