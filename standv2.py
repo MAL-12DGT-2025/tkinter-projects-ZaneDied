04bc72323df759c395380d7a72fddea4f838bdbc
ZaneDied <zane.abad@marcellin.school.nz>
2025-04-03 12:29:30 +1300

    ad

--- standv2.py
+++ standv2.py
@@ -3,9 +3,11 @@ from PIL import Image, ImageTk, ImageOps
 from tkinter import PhotoImage, ttk
 
 root = tk.Tk()
-root.geometry(f'{5*117}x500')
+root.geometry("666x500")
 
 greattitle = ttk.Label(root, text = 'FRUIT MIX').grid(row = 0, column= 2)
+
+padsx = 10
 
 def openimagec(namec,name):
     size = (110, 110)
@@ -15,20 +17,24 @@ def openimagec(namec,name):
         return ImageTk.PhotoImage(namec)
 
 limec = openimagec("limec.png","limec")
-limeimg = ttk.Label(root, image=limec).grid(row = 1, column = 0)
+limeimg = ttk.Label(root, image=limec).grid(row = 2, column = 0, padx=padsx)
 
 lemonc = openimagec("lemonc.png","lemonc")
-lemonimg = ttk.Label(root, image=lemonc).grid(row = 1 , column = 1)
+lemonimg = ttk.Label(root, image=lemonc).grid(row = 2, column = 1, padx=padsx)
 
 strawc = openimagec("strawc.png","strawc")
-strawimg = ttk.Label(root, image=strawc).grid(row = 1 , column = 2)
+strawimg = ttk.Label(root, image=strawc).grid(row = 2, column = 2, padx=padsx)
 
 orangec = openimagec("orangec.png","orangec")
-orangeimg = ttk.Label(root, image=orangec).grid(row = 1 , column = 3)
+orangeimg = ttk.Label(root, image=orangec).grid(row = 2, column = 3, padx=padsx)
 
 applec = openimagec("applec.png","applec")
-appleimg = ttk.Label(root, image=applec).grid(row = 1 , column = 4)
+appleimg = ttk.Label(root, image=applec).grid(row = 2, column = 4, padx=padsx)
 
+
+
+add1 = ttk.Button(root,text = "Add")
+add1.grid(row = 3, column = 0)
 
 
 
@@ -42,7 +48,7 @@ def onmouse(event):
         px.append(event.x)
         py.append(event.y)
         print(px , py)
-        return px,py
+        print(root.winfo_reqheight(), root.winfo_reqwidth())
 
 root.bind('<Button-1>',onmouse)
 
