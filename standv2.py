a017ab154366c2760e6166a8833941f69c74b10e
ZaneDied <zane.abad@marcellin.school.nz>
2025-04-02 13:24:36 +1300

    xtfghz

--- standv2.py
+++ standv2.py
@@ -3,14 +3,7 @@ from PIL import Image, ImageTk, ImageOps
 from tkinter import PhotoImage
 
 root = tk.Tk()
-root.geometry('900x500')
-
-canvas = tk.Canvas(root,width=700,height=500)
-canvas.grid(column=0, row=0)
-canvas.create_oval(285,125,285+130,125+130,fill="#F8CECC",outline="#F8CECC")
-
-
-
+root.geometry('755x500')
 
 size = (110, 110)
 with Image.open("strawberry.png") as im:
@@ -18,11 +11,11 @@ with Image.open("strawberry.png") as im:
     im.save("image_thumbnail.webp")
 
 image = ImageTk.PhotoImage(im)
-canvas.create_image(305,135,image = image)
 
-"""image_label = tk.Label(root, image=image)
+
+image_label = tk.Label(root, image=image)
 image_label.grid(row = 0, column = 0)
-"""
+
 def onmouse(event):
         px = []
         py = []
