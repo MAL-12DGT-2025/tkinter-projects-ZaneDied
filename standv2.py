426604a6deac17ea9161dd5b5d54a68355fae90f
ZaneDied <zane.abad@marcellin.school.nz>
2025-04-03 12:29:23 +1300

    bomboclat

--- standv2.py
+++ standv2.py
@@ -3,32 +3,31 @@ from PIL import Image, ImageTk, ImageOps
 from tkinter import PhotoImage, ttk
 
 root = tk.Tk()
-root.geometry('755x500')
-size = (110, 110)
-oranges = (118,118)
+root.geometry(f'{5*117}x500')
 
-greattitle = ttk.Label(root, text = 'THE GREAT FRUIT').grid(row = 0, column= 2)
+greattitle = ttk.Label(root, text = 'FRUIT MIX').grid(row = 0, column= 2)
 
-def openimagec(namec,size):
+def openimagec(namec,name):
+    size = (110, 110)
     with Image.open(namec) as namec:
         namec.thumbnail(size)
-        namec.save("limepng.webp")
+        namec.save(f"{name}.webp")
         return ImageTk.PhotoImage(namec)
 
-limec = openimagec("limec.png",size)
-limeimg = ttk.Label(root, image=limec).grid(row = 1, column = 0,padx=10)
+limec = openimagec("limec.png","limec")
+limeimg = ttk.Label(root, image=limec).grid(row = 1, column = 0)
 
-lemonc = openimagec("lemonc.png",size)
-lemonimg = ttk.Label(root, image=lemonc).grid(row = 1 , column = 1,padx=10)
+lemonc = openimagec("lemonc.png","lemonc")
+lemonimg = ttk.Label(root, image=lemonc).grid(row = 1 , column = 1)
 
-strawc = openimagec("strawc.png",size)
-strawimg = ttk.Label(root, image=strawc).grid(row = 1 , column = 2,padx=10)
+strawc = openimagec("strawc.png","strawc")
+strawimg = ttk.Label(root, image=strawc).grid(row = 1 , column = 2)
 
-orangec = openimagec("orangec.png",oranges)
-orangeimg = ttk.Label(root, image=orangec).grid(row = 1 , column = 3,padx=10)
+orangec = openimagec("orangec.png","orangec")
+orangeimg = ttk.Label(root, image=orangec).grid(row = 1 , column = 3)
 
-applec = openimagec("applec.png",size)
-appleimg = ttk.Label(root, image=applec).grid(row = 1 , column = 4,padx=10)
+applec = openimagec("applec.png","applec")
+appleimg = ttk.Label(root, image=applec).grid(row = 1 , column = 4)
 
 
 
