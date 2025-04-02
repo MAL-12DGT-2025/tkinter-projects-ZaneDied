e77b6d80d0fcd5af6686fb6ef3977e2826734b2b
ZaneDied <zane.abad@marcellin.school.nz>
2025-04-02 13:03:54 +1300

    asdad

--- standv2.py
+++ standv2.py
@@ -3,24 +3,35 @@ from PIL import Image, ImageTk, ImageOps
 from tkinter import PhotoImage
 
 root = tk.Tk()
-root.geometry('900x600')
+root.geometry('700x500')
 
-canvas = tk.Canvas(root,width=520,height=300)
+canvas = tk.Canvas(root,width=700,height=500)
 canvas.grid(column=0, row=0)
-canvas.create_oval(0,0,150,150,fill="red")
+canvas.create_oval(0,0,150,150,fill="#F8CECC",outline="#F8CECC")
+canvas
 
 
 
-size = (100, 150)
+size = (110, 110)
 with Image.open("strawberry.png") as im:
     im.thumbnail(size)
     im.save("image_thumbnail.webp")
 
 image = ImageTk.PhotoImage(im)
-image_label = tk.Label(root, image=image)
+canvas.create_image(364,191,image = image)
+
+"""image_label = tk.Label(root, image=image)
 image_label.grid(row = 0, column = 0)
+"""
+def onmouse(event):
+        px = []
+        py = []
+        px.append(event.x)
+        py.append(event.y)
+        print(px , py)
+        return px,py
 
-
+root.bind('<Button-1>',onmouse)
 
 
 
