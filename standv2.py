7ce13375f72c58f24786db6ddda0436ec0c2373c
ZaneDied <zane.abad@marcellin.school.nz>
2025-04-03 11:47:12 +1300

    changes images

--- standv2.py
+++ standv2.py
@@ -1,22 +1,37 @@
 import tkinter as tk
 from PIL import Image, ImageTk, ImageOps
-from tkinter import PhotoImage
+from tkinter import PhotoImage, ttk
 
 root = tk.Tk()
 root.geometry('755x500')
 size = (110, 110)
+oranges = (118,118)
+
+greattitle = ttk.Label(root, text = 'THE GREAT FRUIT').grid(row = 0, column= 2)
+
+def openimagec(namec,size):
+    with Image.open(namec) as namec:
+        namec.thumbnail(size)
+        namec.save("limepng.webp")
+        return ImageTk.PhotoImage(namec)
+
+limec = openimagec("limec.png",size)
+limeimg = ttk.Label(root, image=limec).grid(row = 1, column = 0,padx=10)
+
+lemonc = openimagec("lemonc.png",size)
+lemonimg = ttk.Label(root, image=lemonc).grid(row = 1 , column = 1,padx=10)
+
+strawc = openimagec("strawc.png",size)
+strawimg = ttk.Label(root, image=strawc).grid(row = 1 , column = 2,padx=10)
+
+orangec = openimagec("orangec.png",oranges)
+orangeimg = ttk.Label(root, image=orangec).grid(row = 1 , column = 3,padx=10)
+
+applec = openimagec("applec.png",size)
+appleimg = ttk.Label(root, image=applec).grid(row = 1 , column = 4,padx=10)
 
 
 
-with Image.open("limee.png") as limepng:
-     limepng.thumbnail(size)
-     limepng.save("limepng.webp")
-
-limeimage = ImageTk.PhotoImage(limepng)
-
-
-limepng = tk.Label(root, image=limeimage)
-limepng.grid(row = 0, column = 0)
 
 
 
