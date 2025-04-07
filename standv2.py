0af35c5f61cef43aaff678d05614434de3ae5410
ZaneDied <zane.abad@marcellin.school.nz>
2025-04-08 11:44:14 +1200

    asdasd

--- standv2.py
+++ standv2.py
@@ -71,6 +71,41 @@ add5 = ttk.Button(root, text = "Add",style = 'addb.TButton',command = lambda:pre
 add5.grid(row = 3, column = 4, padx=padbx)
 
 
+def open_lime_options():
+    lime_options_window = tk.Toplevel(root)
+    lime_options_window.title("Lime Juice Options")
+
+    ice_label = ttk.Label(lime_options_window, text="Ice:")
+    ice_label.grid(row=0, column=0, padx=5, pady=5)
+    ice_yes_radio = ttk.Radiobutton(lime_options_window, text="Yes", value="yes")
+    ice_yes_radio.grid(row=0, column=1, padx=5, pady=5)
+    ice_no_radio = ttk.Radiobutton(lime_options_window, text="No", value="no")
+    ice_no_radio.grid(row=0, column=2, padx=5, pady=5)
+
+    sugar_label = ttk.Label(lime_options_window, text="Sugar:")
+    sugar_label.grid(row=1, column=0, padx=5, pady=5)
+    sugar_yes_check = ttk.Checkbutton(lime_options_window, text="Add Sugar")
+    sugar_yes_check.grid(row=1, column=1, columnspan=2, padx=5, pady=5)
+
+    # You can add more options here like sweetness level, etc.
+
+    close_button = ttk.Button(lime_options_window, text="Close", command=lime_options_window.destroy)
+    close_button.grid(row=2, column=0, columnspan=3, padx=5, pady=10)
+
+add1 = ttk.Button(root, text="Add", style='addb.TButton', command=open_lime_options)
+add1.grid(row=3, column=0, padx=padbx)
+
+add2 = ttk.Button(root, text="Add", style='addb.TButton')
+add2.grid(row=3, column=1, padx=padbx)
+
+add3 = ttk.Button(root, text="Add", style='addb.TButton')
+add3.grid(row=3, column=2, padx=padbx)
+
+add4 = ttk.Button(root, text="Add", style='addb.TButton')
+add4.grid(row=3, column=3, padx=padbx)
+
+add5 = ttk.Button(root, text="Add", style='addb.TButton')
+add5.grid(row=3, column=4, padx=padbx)
 
 
 """def onmouse(event):
